ea674ffbd7bb9abe751f59cf50badfcc28d9626c
Neves António <90612989+nevesantonio-pilale@users.noreply.github.com>
2022-03-04 17:23:00 +0200

    Exercicio 02 da aula de programação web e mobile

---
+++ resources/views/index.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title> Site em construção </title>
+    <style>
+
+        * {
+            margin: 0;
+            padding: 0;
+        }
+        .bng {
+            background-color: black;
+            height: 100vh;
+            width: 100%;
+            margin: 0 auto;
+        }
+
+        .bng img {
+            width: 100%;
+            height: 100%;
+            opacity: 50%;
+        }
+
+        .relogio {
+            position: absolute;
+            top: 15%;
+            width: 200px;
+            height: 200px;
+            left: 20%;
+            border-radius: 100%;
+            background-color: #9999;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+
+            color: #FFFFFF;
+        }
+
+
+    </style>
+</head>
+<body>
+    <div class="bng">
+        <img src="site_em_construcao.jpg" alt="imagem de site em contrução">
+
+        <div class="relogio">
+            <h2> Até Breve! </h2>
+        </div>
+
+    </div>
+</body>
+</html>
